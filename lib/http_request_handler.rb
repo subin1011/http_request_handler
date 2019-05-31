ec0920fc3a09e6cfa9371c85602cac72fa363b32
Subin Dev <subin@dronamobile.com>
2019-05-31 17:32:17 +0530

    init http_request_handler gem

---
+++ lib/http_request_handler.rb
@@ -0,0 +1,6 @@
+require "http_request_handler/version"
+
+module HttpRequestHandler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/http_request_handler/version.rb
@@ -0,0 +1,3 @@
+module HttpRequestHandler
+  VERSION = "0.1.0"
+end
